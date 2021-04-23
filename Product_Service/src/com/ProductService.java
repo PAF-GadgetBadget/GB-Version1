4ac85b8b482149ba2a4b8fabb3c7bd6123b261a3
hasinii98 <hasininimaya@gmail.com>
2021-04-23 10:19:43 +0530

    ProductService - new update

--- Product_Service/src/com/ProductService.java
+++ Product_Service/src/com/ProductService.java
@@ -13,14 +13,30 @@ import org.jsoup.nodes.Document;
 @Path("/Product") 
 public class ProductService 
 { 
+
+@POST
+@Path("/") 
+@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 
+@Produces(MediaType.TEXT_PLAIN) 
+public String insertProduct(@FormParam("pTitle") String title, 
+@FormParam("pDesc") String desc, 
+@FormParam("pPrice") String price, 
+@FormParam("resName") String name,
+@FormParam("date") String date)
+
+{ 
+String output = productObj.insertProduct(title, desc, price, name, date); 
+return output; 
+}
+
+
 Product productObj = new Product(); 
 @GET
-@Path("/getProduct") 
+@Path("/") 
 @Produces(MediaType.TEXT_HTML) 
 public String readProducts() 
 {     
 return productObj.readProducts(); 
 } 
 
-
-//Get method
+}
