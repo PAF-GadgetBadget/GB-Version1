f979671a75fd936b8a99e2e0d2a56b20f877a202
Danula Wanasinghe <57353079+danulawa@users.noreply.github.com>
2021-04-25 22:33:59 +0530

    SQL code done

--- Database.sql
+++ Database.sql
@@ -1,1 +1,196 @@
-This is the first line
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: gbdatabase
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customer` (
+  `custId` int NOT NULL AUTO_INCREMENT,
+  `custName` varchar(45) NOT NULL,
+  `user_user_id` int NOT NULL,
+  PRIMARY KEY (`custId`,`user_user_id`),
+  KEY `fk_customer_user_idx` (`user_user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` VALUES (1,'sunil',1),(2,'nimal',2);
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fund`
+--
+
+DROP TABLE IF EXISTS `fund`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fund` (
+  `fundId` int NOT NULL AUTO_INCREMENT,
+  `researcherId` int NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `fundingAmount` int NOT NULL,
+  `funderName` varchar(100) NOT NULL,
+  PRIMARY KEY (`fundId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fund`
+--
+
+LOCK TABLES `fund` WRITE;
+/*!40000 ALTER TABLE `fund` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fund` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `order_id` int NOT NULL AUTO_INCREMENT,
+  `customer_customer_id` int NOT NULL,
+  `project_project_id` int NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`order_id`,`customer_customer_id`,`project_project_id`),
+  KEY `fk_orders_customer1_idx` (`customer_customer_id`),
+  KEY `fk_orders_project1_idx` (`project_project_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (4,1,2,'2021-04-23');
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `payment`
+--
+
+DROP TABLE IF EXISTS `payment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `payment` (
+  `payment_id` int NOT NULL AUTO_INCREMENT,
+  `card_type` varchar(45) NOT NULL,
+  `card_number` varchar(45) NOT NULL,
+  `name_on_card` varchar(45) NOT NULL,
+  `cvc` varchar(45) NOT NULL,
+  `expire_date` date NOT NULL,
+  `status` varchar(45) NOT NULL,
+  `sub_amount` double NOT NULL,
+  `date` date NOT NULL,
+  `tax_tax_id` int NOT NULL,
+  `order_order_id` int NOT NULL,
+  PRIMARY KEY (`payment_id`,`tax_tax_id`,`order_order_id`),
+  KEY `fk_payment_tax1_idx` (`tax_tax_id`),
+  KEY `fk_payment_order1_idx` (`order_order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `payment`
+--
+
+LOCK TABLES `payment` WRITE;
+/*!40000 ALTER TABLE `payment` DISABLE KEYS */;
+INSERT INTO `payment` VALUES (4,'visa','2345678','kmal','150','2023-12-30','pending',20100,'2021-04-11',3,1),(5,'master','4567889','susantha','140','2021-12-30','pending',34000,'2021-04-21',4,2),(6,'visa','2398989','vimal','159','2023-10-30','pending',30100,'2021-03-11',3,2),(7,'visa','2345678','kmal','150','2023-12-30','pending',20100,'2021-04-11',3,1);
+/*!40000 ALTER TABLE `payment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `pId` int NOT NULL AUTO_INCREMENT,
+  `pTitle` varchar(45) DEFAULT NULL,
+  `pDesc` varchar(100) DEFAULT NULL,
+  `pPrice` float DEFAULT NULL,
+  `resName` varchar(45) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`pId`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (5,'vsvnnvk','vdm',45678,'memem','2021-04-02'),(6,'hh','we',448,'vzdj','2021-09-06'),(7,'sdkljg','twy',78,'jatt','2021-01-02');
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tax`
+--
+
+DROP TABLE IF EXISTS `tax`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tax` (
+  `tax_id` int NOT NULL AUTO_INCREMENT,
+  `tax_amount` float NOT NULL,
+  `valid_from` date NOT NULL,
+  `valid_to` date NOT NULL,
+  PRIMARY KEY (`tax_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tax`
+--
+
+LOCK TABLES `tax` WRITE;
+/*!40000 ALTER TABLE `tax` DISABLE KEYS */;
+INSERT INTO `tax` VALUES (2,300,'2021-01-03','2022-02-03'),(3,100,'2021-02-03','2022-02-03'),(4,4000,'2021-04-11','2024-04-11');
+/*!40000 ALTER TABLE `tax` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-04-25 22:26:36
